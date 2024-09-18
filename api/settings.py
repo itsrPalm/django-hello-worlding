6b74904f9adb2db1c2782c6acc66354cd52a270c
DripTrace <rpalm@driptrace.io>
2024-09-18 14:05:25 -0700

    temp removing static

--- api/settings.py
+++ api/settings.py
@@ -92,7 +92,7 @@ TEMPLATES = [
 
 # WSGI_APPLICATION = 'api.wsgi.app'
 
-WSGI_APPLICATION = 'api.wsgi.application'
+WSGI_APPLICATION = 'api.wsgi.app'
 
 
 
@@ -185,10 +185,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- api/wsgi.py
+++ api/wsgi.py
@@ -1,4 +1,12 @@
-from api.wsgi import application
+# from api.wsgi import application
 
-# This is the important part for Vercel
-app = application
+# # This is the important part for Vercel
+# app = application
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+app = get_wsgi_application()
