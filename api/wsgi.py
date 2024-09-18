99faac26d001e48340905b1ab162f7a6be6d7df6
DripTrace <rpalm@driptrace.io>
2024-09-18 13:13:49 -0700

    changing

--- api/wsgi.py
+++ api/wsgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+# app = get_wsgi_application()
+
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+application = WhiteNoise(application)
